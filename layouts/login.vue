7bf9e81db3555fc00c496b9c6ff856ef4e7b256e
Jatin Katyal <jatin.katyal13@gmail.com>
2020-04-09 17:06:01 +0530

    add login page

---
+++ layouts/login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <NavBar />
+    <div class="main-content">
+      <nuxt  />
+    </div>
+  </div>
+</template>
+
+<script>
+import NavBar from '~/components/Login/NavBar'
+
+export default {
+  components: {
+    NavBar
+  },
+  head: {
+    bodyAttrs: {
+      class: 'bg-default'
+    }
+  }
+}
+</script>
---
+++ pages/login.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="main-content">
+    <!-- Header -->
+    <div class="header bg-gradient-primary py-7 py-lg-8 pt-lg-9">
+      <div class="container">
+        <div class="header-body text-center mb-7">
+          <div class="row justify-content-center">
+            <div class="col-xl-5 col-lg-6 col-md-8 px-5">
+              <h1 class="text-white">Welcome!</h1>
+              <p class="text-lead text-white">Use these awesome forms to login or create new account in your project for free.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="separator separator-bottom separator-skew zindex-100">
+        <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
+          <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
+        </svg>
+      </div>
+    </div>
+    <!-- Page content -->
+    <div class="container mt--8 pb-5">
+      <div class="row justify-content-center">
+        <div class="col-lg-5 col-md-7">
+          <div class="card bg-secondary border-0 mb-0">
+            <div class="card-header bg-transparent pb-5">
+              <div class="text-muted text-center mt-2 mb-3"><small>Sign in with</small></div>
+              <div class="btn-wrapper text-center">
+                <a href="#" class="btn btn-neutral btn-icon">
+                  <span class="btn-inner--icon"><img src="/img/icons/common/linkedin.png"></span>
+                  <span class="btn-inner--text">LinkedIn</span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'login'
+}
+</script>
